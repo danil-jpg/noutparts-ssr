71571696557c6eca389fe1b51a852080292add0e
Danil <danil.polovinkin.1994@gmail.com>
2023-11-28 12:14:03 +0200

    added some drafts on catalogue links

--- app/common/components/CatalogueItem/CatalogueItem.tsx
+++ app/common/components/CatalogueItem/CatalogueItem.tsx
@@ -27,45 +27,47 @@ export default function CatalogueItem({ image, query, mobImage }: ICatalogueItem
     }, [query]);
 
     return (
-        <div className='catalogue-item'>
-            <div className='catalogue-item__top'>
-                {/*  @ts-ignore: Object is possibly 'null'. */}
-                <Link href={`catalogue/filter-page?${query.match(/^([^\s?]+)(?=\?)/)[0]}`}>
-                    <Image
-                        className='catalogue-item__top_img'
-                        fill={true}
-                        src={image}
-                        sizes='(max-width: 768px) 200px,200px'
-                        alt='catalogue-item'
-                    />
-                    <Image
-                        className='catalogue-item__top_img catalogue-item__top_img_mob'
-                        fill={true}
-                        src={mobImage}
-                        alt='catalogue-item'
-                    />
-                </Link>
+        <Suspense fallback={<Loading></Loading>}>
+            <div className='catalogue-item'>
+                <div className='catalogue-item__top'>
+                    {/*  @ts-ignore: Object is possibly 'null'. */}
+                    <Link href={`catalogue/filter-page?${query.match(/^([^\s?]+)(?=\?)/)[0]}`}>
+                        <Image
+                            className='catalogue-item__top_img'
+                            fill={true}
+                            src={image}
+                            sizes='(max-width: 768px) 200px,200px'
+                            alt='catalogue-item'
+                        />
+                        <Image
+                            className='catalogue-item__top_img catalogue-item__top_img_mob'
+                            fill={true}
+                            src={mobImage}
+                            alt='catalogue-item'
+                        />
+                    </Link>
+                </div>
+                <div className='catalogue-item__main'>
+                    <ul className='catalogue-item__main_ul'>
+                        {res?.data
+                            ? res.data.map((el) => (
+                                  <Link
+                                      key={el.id}
+                                      href={`catalogue/filter-page?${
+                                          query.match(/^([^\s?]+)(?=\?)/)[0]
+                                      }&filters[brand][$eq]=${el.attributes.brand}`}>
+                                      <li
+                                          key={el.id}
+                                          data-modal={el.id}
+                                          className='catalogue-item__main_li'>
+                                          {el.attributes.brand}
+                                      </li>
+                                  </Link>
+                              ))
+                            : ''}
+                    </ul>
+                </div>
             </div>
-            <div className='catalogue-item__main'>
-                <ul className='catalogue-item__main_ul'>
-                    {res
-                        ? res.data.map((el) => (
-                              //   <Link
-                              //       key={el.id}
-                              //       href={`catalogue/filter-page?${
-                              //           query.match(/^([^\s?]+)(?=\?)/)[0]
-                              //       }`}>
-                              <li
-                                  key={el.id}
-                                  data-modal={el.id}
-                                  className='catalogue-item__main_li'>
-                                  {el.attributes.brand}
-                              </li>
-                              //   </Link>
-                          ))
-                        : ''}
-                </ul>
-            </div>
-        </div>
+        </Suspense>
     );
 }
