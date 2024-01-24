c25f6813b5f0290b856e882c46bae9c8301442ef
Danil <danil.polovinkin.1994@gmail.com>
2024-01-24 12:25:37 +0200

    reworked cards on mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "642c40268bf439af"; }
+/******/ 		__webpack_require__.h = function() { return "e5c8d918ff862ec9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
