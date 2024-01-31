30455869ae976ab7ee83be6b90149f6018c7b5c5
Pavlo <tishkovets.pavlo@gmail.com>
2024-01-31 13:44:42 +0200

    removed scrolls where they are not needed

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b5747ca94a660f1b")
+/******/ 		__webpack_require__.h = () => ("136ec1f3bb2404eb")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6a1b770d40ced5be"; }
+/******/ 		__webpack_require__.h = function() { return "9ee90f2dcc6655a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
